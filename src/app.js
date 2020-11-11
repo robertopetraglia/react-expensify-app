7f7cf92693bce8e851a4caac0e9a5ea3d6dfa1ba
robpet <robertopetraglia1983@gmail.com>
2020-11-11 22:10:21 +0100

    Add Login/Login Auth with Google Auth Provider

--- src/app.js
+++ src/app.js
@@ -1,24 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from 'react-redux'
-import AppRouter from './routers/AppRouter'
+import AppRouter, { history } from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import { startSetExpense } from './actions/expenses'
 import "./styles/style.scss"
 import "normalize.css/normalize.css"
 import 'react-dates/lib/css/_datepicker.css'
-import './firebase/firebase'
+import { firebase} from './firebase/firebase'
 
 const store = configureStore()
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
     </Provider>
 )
+let hasRendered = false
+const renderApp = () => {
+    if (!hasRendered) {
+        ReactDOM.render(jsx, document.getElementById("app"));
+        hasRendered = true
+    }
+}
 
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
-
-store.dispatch(startSetExpense()).then(() => {
-    ReactDOM.render(jsx, document.getElementById("app"));
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        console.log('log in')
+        store.dispatch(startSetExpense()).then(() => {
+            renderApp()
+            if (history.location.pathname === '/') {
+                history.push('/dashboard')
+                console.log('ppp')
+            }
+        })
+    } else {
+        console.log('log out')
+        renderApp()
+        history.push('/')
+    }
 })
